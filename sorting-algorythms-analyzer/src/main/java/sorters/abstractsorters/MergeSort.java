abde1b21031e17bfb6dee61951e4d274cdbdbca6
vladivashchenko <vladviva16@gmail.com>
2019-01-19 19:14:20 +0200

    Added parallelism to merge sort

--- sorting-algorythms-analyzer/src/main/java/sorters/abstractsorters/MergeSort.java
+++ sorting-algorythms-analyzer/src/main/java/sorters/abstractsorters/MergeSort.java
@@ -1,4 +1,13 @@
 package sorters.abstractsorters;
+
+import sorters.MergeSortRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author Vlad Ivashchenko
  *
@@ -10,68 +19,104 @@ package sorters.abstractsorters;
 public abstract class MergeSort extends Sorter {
     /**
      * Merging method that usually used to merge sorted parts of array.
-     * @param arr Array which is needed to be sorted
-     * @param l - Left index
-     * @param r - Right index
-     * @param m - Middle index
+     *
+     * @param array Array which is needed to be sorted
+     * @param left   - Left index
+     * @param right   - Right index
+     * @param middle   - Middle index
      */
-    protected void merge(int arr[], int l, int m, int r)
-    {
-        int n1 = m - l + 1;
-        int n2 = r - m;
+    private synchronized void merge(int array[], int left, int middle, int right) {
+        int leftSize = middle - left + 1;
+        int rightSize = right - middle-1;
 
-        int L[] = new int [n1];
-        int R[] = new int [n2];
+        int leftArray[] = new int[leftSize];
+        int rightArray[] = new int[rightSize];
 
-        for (int i=0; i<n1; ++i)
-            L[i] = arr[l + i];
-        for (int j=0; j<n2; ++j)
-            R[j] = arr[m + 1+ j];
+        for (int i = 0; i < leftSize; ++i)
+            leftArray[i] = array[left + i];
+        for (int j = 0; j < rightSize; ++j)
+            rightArray[j] = array[middle + 1 + j];
 
         int i = 0, j = 0;
 
-        int k = l;
-        while (i < n1 && j < n2)
-        {
-            if (L[i] <= R[j])
-            {
-                arr[k] = L[i];
+        int k = left;
+        while (i < leftSize && j < rightSize) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
                 i++;
-            }
-            else
-            {
-                arr[k] = R[j];
+            } else {
+                array[k] = rightArray[j];
                 j++;
             }
             k++;
         }
 
-        while (i < n1)
-        {
-            arr[k] = L[i];
+        while (i < leftSize) {
+            array[k] = leftArray[i];
             i++;
             k++;
         }
 
-        while (j < n2)
-        {
-            arr[k] = R[j];
+        while (j < rightSize) {
+            array[k] = rightArray[j];
             j++;
             k++;
         }
     }
     /**
-     * Method that used to copy elements from one array to another.
-     * @param source Source array which elements must be copied
-     * @param begin - Begin index
-     * @param end - End index
-     * @return int[] Result array
+     * Merging method that used to merge sorted parts of array in parallel.
+     *
+     * @param array Array which is needed to be sorted
+     * @param threadCount   - Count of threads
+     * @param sorter   - Sorter to sort halves
      */
-    protected int[] copyArray(int[]source, int begin,int end){
-        int[] array = new int[end-begin];
-        for(int i=begin;i<end;i++)
-            for(int j=0;j<array.length-1;j++)
-                array[j]=source[i];
-        return array;
+    public void parallelMergeSort(int[] array, final int threadCount,Sorter sorter) {
+        if (threadCount <= 1) {
+            mergeSort(array,sorter);
+        } else if (array.length >= 2) {
+            int leftIndex =0;
+            int rightIndex = array.length;
+            int middleIndex= (rightIndex-leftIndex)/2;
+          
+            int[] left = Arrays.copyOfRange(array, leftIndex, middleIndex);
+            int[] right = Arrays.copyOfRange(array, middleIndex, rightIndex);
+
+            Thread lThread = new Thread(new MergeSortRunner(left, threadCount -1,this,sorter));
+            Thread rThread = new Thread(new MergeSortRunner(right, threadCount -1,this,sorter));
+            lThread.start();
+            rThread.start();
+
+            try {
+                lThread.join();
+                rThread.join();
+            } catch (InterruptedException ie) {
+            }
+            
+            merge(array,leftIndex,middleIndex, rightIndex);
+        }
+    }
+
+    /**
+     * Method that arranges the elements of the given array into sorted order
+     * using the "merge sort" algorithm, which splits the array in half,
+     * sorts the halves, then merges the sorted halves
+     *
+     * @param array Array which is needed to be sorted
+     * @param sorter   - Sorter to sor halves
+     */
+    private synchronized void mergeSort(int[] array, Sorter sorter) {
+        if (array.length >= 2) {
+            int leftIndex =0;
+            int rightIndex = array.length;
+            int middleIndex= (rightIndex-leftIndex)/2;
+
+            int[] left  = Arrays.copyOfRange(array, 0, array.length / 2);
+            int[] right = Arrays.copyOfRange(array, array.length / 2, array.length);
+
+            sorter.sort(left);
+            sorter.sort(right);
+
+            merge(array,leftIndex,middleIndex, rightIndex);
+        }
     }
 }
